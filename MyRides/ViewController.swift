8d88b12972b59b60fb108862a5c21aea08f93490
Ralston <rally619@gmail.com>
2024-06-28 10:32:52 -0400

    inital commit

---
+++ MyRides/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyRides
+//
+//  Created by Ralston Goes on 6/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
